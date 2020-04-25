b43a95bd8839361d1d874590e118950f50ba34fa
anurag <anurag.fichadiya7@gmail.com>
2020-04-25 14:52:20 +0530

    short path

--- src/app/components/customize/customize.component.ts
+++ src/app/components/customize/customize.component.ts
@@ -3,6 +3,7 @@ import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
 //import { DocumentEditorComponent } from '@syncfusion/ej2-angular-documenteditor';
 import * as jsPDF from 'jspdf'
+import { DataService } from 'src/shared/services/data.service';
 @Component({
   selector: 'app-customize',
   templateUrl: './customize.component.html',
@@ -12,10 +13,9 @@ import * as jsPDF from 'jspdf'
 export class CustomizeComponent implements OnInit {
   
   formattedAddress : Address;
-
   addressvec : Address[] = [ ];    
   //constructor(public docEdit : DocumentEditorComponent) { }
-  constructor( ) { }
+  constructor(public dataService : DataService) { }
   ngOnInit() {
   }
   placesRef : GooglePlaceDirective;
@@ -28,14 +28,67 @@ export class CustomizeComponent implements OnInit {
     this.formattedAddress = address;
     const t = address
     this.addressvec.push(t)
-    console.log(this.formattedAddress)
-    console.log("this.formattedAddress", this.formattedAddress.photos[0].getUrl)
   }
-
+  //https://stackoverflow.com/questions/1859185/how-to-force-sequential-javascript-execution
+  public shortest_path () 
+  {
+    let a : string[] = [ ];
+    let send : string = '';
+    this.addressvec.forEach((t) => {
+      send = send + t.id + '&' + t.geometry.location.lat() + '&' + t.geometry.location.lng() + '&'
+    })
+    this.dataService.getpy('path/'+send).subscribe((data) => {
+      let addressvecf : Address [] = [ ];
+      console.log(data)
+      a = this.anu(data);
+      a = this.anu(a)
+      console.log("a after anu fn", a);
+      a.forEach(it => {
+        let addressvect = this.addressvec;
+        addressvect.forEach((item) => {
+          console.log("item.id ",item.id)
+          console.log("it", it)
+          if(item.id == it)
+          {
+            addressvecf.push(item);
+          }
+        })
+        console.log("this.addressvecf", addressvecf)
+        //console.log("this.addressvecf", this.addressvecf)
+      })
+      this.addressvec = addressvecf;
+      console.log("this.addressvec", this.addressvec)
+    })
+   
+  }
+  anu (value: any, args?: any[]): any[] 
+  {
+    // check if "routes" exists
+    if(value) {
+      // create instance vars to store keys and final output
+      let keyArr: any[] = Object.keys(value);
+      let dataArr :string[] = [];
+      // loop through the object,
+      // pushing values to the return array
+      keyArr.forEach((key: any) => {
+          dataArr.push(value[key]);
+          console.log("This if from anu fn : key", key);
+          console.log("This if from anu fn : val", value[key]);
+      });
+      // return the resulting array
+      return dataArr;
+    }
+  }
   removeItem (add : Address)
   {
     this.addressvec.splice(this.addressvec.indexOf(add), 1)
   }
+
+  gen_button()
+  {
+    this.shortest_path();
+  }
+
   @ViewChild('content', {static: false}) content : ElementRef
   geniti()
   {
